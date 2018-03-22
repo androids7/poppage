65396c758456ed33252adecc5d432dd38622dfa1
androids7 <1559215761@qq.com>
2018-03-22 19:12:19 +0800

    ff

--- FragmentLifeCycle-master/app/src/main/java/com/jzj1993/fragmentlifecycle/MainActivity.java
+++ FragmentLifeCycle-master/app/src/main/java/com/jzj1993/fragmentlifecycle/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.*;
+import android.view.View.*;
+import android.view.*;
+import android.graphics.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private BFragment mBFragment = new BFragment();
     private CFragment mCFragment = new CFragment();
 
+	
+	private ImageView home,message;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +31,11 @@ public class MainActivity extends AppCompatActivity {
             public Fragment getItem(int position) {
                 switch (position) {
                     case 0:
+						home.setColorFilter(Color.argb(25,0,100,100));
                         return mAFragment;
                     case 1:
+						home.setColorFilter(Color.argb(0,0,0,0));
+						
                         return mBFragment;
                     case 2:
                         return mCFragment;
@@ -39,5 +49,17 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mPager.setAdapter(mAdapter);
+		
+		home=(ImageView)findViewById(R.id.activitymainImageViewhome);
+		
+		message=(ImageView)findViewById(R.id.activitymainImageViewmessage);
+		
+		home.setOnClickListener(new OnClickListener(){
+		public void onClick(View v)
+		{
+			
+		}
+			
+		});
     }
 }
